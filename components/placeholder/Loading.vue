45cca21068c241d672c7267b2a896b4e7638617b
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-07-21 16:14:31 +0300

    enhance loader

--- components/placeholder/Loading.vue
+++ components/placeholder/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center gap-2 h-[150px] border-2 border-dashed border-blue text-xl text-blue bg-blue-50 py-2 px-4"
+    class="flex flex-col items-center justify-center gap-2 h-[150px] text-xl text-blue py-2 px-4"
   >
     <v-avatar color="blue" icon="mdi-loading" size="x-large" class="motion !text-white mb-2" />
     Loading {{ name }}!
