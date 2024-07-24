c75673118e7d01a7e4479f371cb7cdbbb75cad45
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-07-25 01:07:27 +0300

    change url

--- composables/api.ts
+++ composables/api.ts
@@ -3,8 +3,8 @@ export const api = (url: string, options?: any) => {
 
   const accessToken = useCookie('5d_access_token');
 
-  const BASE_URL = 'http://localhost:8000/api';
-  // const BASE_URL = 'https://5delivery.store/DeliveryApp/public/api'
+  // const BASE_URL = 'http://localhost:8000/api';
+  const BASE_URL = 'https://5delivery.store/DeliveryApp/public/api'
 
   return $fetch(BASE_URL + url, {
     ...options,
