1defde28121dd05b4f319d998107f23d8a60ebab
yasser-jam <63106211+yasser-jam@users.noreply.github.com>
2024-08-14 07:04:06 +0000

    Build: (11d55d3) clean tree

---
+++ _nuxt/BzUGQ57P.js
@@ -0,0 +1,1 @@
+import{n as e}from"./B1X0TXc5.js";import{m as n,N as o,O as r}from"./BD5YuzA9.js";const i=n({__name:"index",setup(t){return e("/drivers"),(a,s)=>(o(),r("div"))}});export{i as default};
