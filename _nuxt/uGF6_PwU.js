3a8eac41404560b318fee38fafae716aa2b39813
yasser-jam <63106211+yasser-jam@users.noreply.github.com>
2024-08-18 18:50:28 +0000

    Build: (63df0a1) target position

---
+++ _nuxt/uGF6_PwU.js
@@ -0,0 +1,1 @@
+import{n as e}from"./uaJhe2UR.js";import{m as n,N as o,O as r}from"./BD5YuzA9.js";const i=n({__name:"index",setup(t){return e("/drivers"),(a,s)=>(o(),r("div"))}});export{i as default};
