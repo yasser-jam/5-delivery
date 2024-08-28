b0d449ccc1314f17824fa6448a190de09baf4fd4
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-08-28 08:25:54 +0300

    stop middleware test

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,10 +1,10 @@
 export default defineNuxtRouteMiddleware((from, to) => {
     const accessToken = useCookie('5d_access_token')
 
-    if (from.path == '/login') return true
-    if (to.path == '/login') return true
+    // if (from.path == '/login') return true
+    // if (to.path == '/login') return true
 
 
 
-    if (!accessToken.value) return navigateTo('/login')
+    // if (!accessToken.value) return navigateTo('/login')
 })
